3119a1a339c03133ad7427d7ced188310028cf09
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-05-23 12:38:17 -0400

    a

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,24 @@
-# This is an _environment variable_ that is used by some of the Rake tasks to determine
-# if our application is running locally in development, in a test environment, or in production
-ENV['RACK_ENV'] ||= "development"
-
-# Require in Gems
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require
+configure :development do
+ ENV['SINATRA_ENV'] ||= "development"
+require 'bundler/setup'
+ Bundler.require(:default, ENV['SINATRA_ENV'])
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+ )
+end
 
-# Require in all files in 'app' directory
-require_all 'app'
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
